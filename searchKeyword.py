2a9dd54ab9eeff9773fe6013f7e45733b0b50f46
liran319 <liran319@gmail.com>
2014-10-21 00:21:08 +0800

    demo

--- searchKeyword.py
+++ searchKeyword.py
@@ -5,5 +5,19 @@ import re
 import urllib2
 
 
-def function():
-    pass
+class KeyWordParser(object):
+    """docstring for ClassName"""
+    def __init__(self, keyword):
+        self.keyword = keyword
+    
+    def Youku(self):
+        search_addr = r"http://www.soku.com/v?keyword=" + self.keyword
+        return search_addr
+
+a = KeyWordParser('火影')
+
+url = a.Youku()
+htmlpage = urllib2.urlopen(url).read()
+
+print url
+print htmlpage
